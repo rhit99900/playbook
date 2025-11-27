ee8c4e9492b1ab60e2abe50411c0905178ff9267
Rohit Nair <rohitcse081@gmail.com>
2025-11-27 12:35:05 +0530

    Fixes for Root

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from "next";
+const path = require('path');
 
 const nextConfig: NextConfig = {
   env: {
     API_BASE_URI: process.env.API_BASE_URI,
     PORT: process.env.APPLICATION_PORT
-  }
+  },  
+  outputFileTracingRoot: path.join(__dirname, '')
 };
 
 export default nextConfig;
